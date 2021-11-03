7a02425b65586cba6a507126bf45f42a0ff5490f
Christopher Boyd <mikuana@gmail.com>
2021-11-02 21:27:50 -0700

    Restructure to suit me

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@
 
 """The setup script."""
 
+import re
+from pathlib import Path
+
 from setuptools import setup, find_packages
 
 with open("README.rst") as readme_file:
@@ -9,6 +12,20 @@ with open("README.rst") as readme_file:
 
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
+
+
+def read_version():
+    try:
+        v = [
+            x
+            for x in Path("oops_fhir/__init__.py").open()
+            if x.startswith("__version__")
+        ][0]
+        v = re.match(r"__version__ *= *'(.*?)'\n", v)[1]
+        return v
+    except Exception as e:
+        raise RuntimeError(f"Unable to read version string: {e}")
+
 
 requirements = []
 
@@ -29,8 +46,9 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
-    description="Object-Oriented Programming Support for Fast Healthcare InteroperResources",
+    description="Object-Oriented Programming Support for Fast Healthcare Interopere Resources (FHIR)",
     install_requires=requirements,
     license="MIT license",
     long_description=readme + "\n\n" + history,
