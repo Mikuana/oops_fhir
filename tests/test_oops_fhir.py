c11d8ba8d6a8d90bd1c7b47d78c8741cc5488855
Christopher Boyd <mikuana@gmail.com>
2022-01-19 16:43:26 -0800

    Getting ready for first release

--- tests/test_oops_fhir.py
+++ tests/test_oops_fhir.py
@@ -2,23 +2,7 @@
 
 """Tests for `oops_fhir` package."""
 
-import pytest
 
-
-from oops_fhir import oops_fhir
-
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
+def test_test():
     """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+    assert True
